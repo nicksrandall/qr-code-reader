d24a4ea1411547d5941413da40a0ac00fb589fc2
Nick Randall <nicksrandall@gmail.com>
2019-05-17 16:49:16 -0400

    Don't wait for window load

--- src/main.js
+++ src/main.js
@@ -7,10 +7,7 @@ const app = new App({
 
 // Check that service workers are registered
 if ("serviceWorker" in navigator) {
-  // Use the window load event to keep the page load performant
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js");
-  });
+  navigator.serviceWorker.register("/service-worker.js");
 }
 
 export default app;
